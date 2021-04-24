00dd6457c623e7525f1e2ba87175c44d1e32e4d4
Boris <boris.sch@gmail.com>
2021-04-24 13:19:15 +0300

    added few comments

--- src/main.cpp
+++ src/main.cpp
@@ -49,16 +49,9 @@
 //     }
 // }
 
-
-
-// CPP code for bubble sort
-// using template function
 #include <iostream>
 using namespace std;
 
-// A template function to implement bubble sort.
-// We can use this for any data type that supports
-// comparison operator < and swap works for it.
 template <class T, class U>
 void bubbleSort(T a[], U n) {
     cout << a[1];
@@ -71,9 +64,6 @@ int main() {
     int a[5] = {10, 50, 30, 40, 20};
     char ch = 'a';
     int n = sizeof(a) / sizeof(a[0]);
-
-    // calls template function
     bubbleSort<int,char>(a, ch);
-
   return 0;
 }
